7fc514bc029867c143ae2cb542491e80fd6036df
Mateusz <migdalmateusz@wp.pl>
2021-04-19 13:41:57 +0200

    add dto package

--- src/main/java/com/migdal/songcupwebapplication/spotifyapi/SpotifyAlbumClient.java
+++ src/main/java/com/migdal/songcupwebapplication/spotifyapi/SpotifyAlbumClient.java
@@ -1,6 +1,7 @@
 package com.migdal.songcupwebapplication.spotifyapi;
 
 import com.migdal.songcupwebapplication.spotifyapi.model.SpotifyAlbum;
+import com.migdal.songcupwebapplication.spotifyapi.model.dto.SpotifyAlbumDto;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,22 +13,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class SpotifyAlbumClient {
 
     @GetMapping("/album/{authorName}")
-    public SpotifyAlbum getAlbumsByAuthor(OAuth2Authentication details, @PathVariable String authorName) {
-        String jwt = ((OAuth2AuthenticationDetails)details.getDetails()).getTokenValue();
+    public List<SpotifyAlbumDto> getAlbumsByAuthor(OAuth2Authentication details, @PathVariable String authorName) {
+        String jwt = ((OAuth2AuthenticationDetails) details.getDetails()).getTokenValue();
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", "Bearer " + jwt);
         HttpEntity httpEntity = new HttpEntity(httpHeaders);
 
-        ResponseEntity<SpotifyAlbum> exchange = restTemplate.exchange("https://api.spotify.com/v1/search?q="+ authorName + "&type=track&market=US&limit=10&offset=5",
+        ResponseEntity<SpotifyAlbum> exchange = restTemplate.exchange("https://api.spotify.com/v1/search?q=" + authorName + "&type=track&market=US&limit=10&offset=5",
                 HttpMethod.GET,
                 httpEntity,
                 SpotifyAlbum.class);
-        return exchange.getBody();
+
+        List<SpotifyAlbumDto> spotifyAlbumDtoList =
+                exchange.getBody().
+                        getTracks().getItems().stream().
+                        map(item -> new SpotifyAlbumDto(item.getName(), item.getAlbum().getImages().get(0).getUrl()))
+                        .collect(Collectors.toList());
+
+        return spotifyAlbumDtoList;
     }
 }
---
+++ src/main/java/com/migdal/songcupwebapplication/spotifyapi/model/dto/SpotifyAlbumDto.java
@@ -0,0 +1,34 @@
+package com.migdal.songcupwebapplication.spotifyapi.model.dto;
+
+public class SpotifyAlbumDto {
+
+    private String trackName;
+    private String imageUrl;
+
+    public SpotifyAlbumDto(String trackName, String imageUrl) {
+        this.trackName = trackName;
+        this.imageUrl = imageUrl;
+    }
+
+    public SpotifyAlbumDto() {
+    }
+
+    public SpotifyAlbumDto(String name) {
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
